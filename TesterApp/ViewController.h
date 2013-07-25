39384bc82a32f9d900b895eb04832290be8a13da
Naoki Ogishi <naoki.ogishi@gmail.com>
2013-07-24 20:48:42 -0700

    Initial Commit

---
+++ TesterApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TesterApp
+//
+//  Created by Ogishi on 7/24/13.
+//  Copyright (c) 2013 Ogishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
